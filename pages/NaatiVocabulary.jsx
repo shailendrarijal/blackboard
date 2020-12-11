3e282219b94f934333d285fa8fbd6928cac86b04
shailendrarijal <45778804+shailendrarijal@users.noreply.github.com>
2020-12-11 18:29:24 +1100

    Revert "update-vocab-file+tips"

--- pages/NaatiVocabulary.jsx
+++ pages/NaatiVocabulary.jsx
@@ -1,7 +1,200 @@
 import { useState, useEffect } from "react"
-import vocab from "./vocab";
+
 export default function NaatiVocabulary() {
-  
+  const vocab = {
+    business: [
+      {
+        id: 1,
+        englishWord: "business",
+        nepaliWord: "व्यापार"
+      },
+      {
+        id: 2,
+        englishWord: "advertising campaign",
+        nepaliWord: "बिज्ञापन अभियान"
+      },
+      {
+        id: 3,
+        englishWord: "branch office",
+        nepaliWord: "शाखा कार्यालय"
+      }],
+    education: [
+      {
+        id: 1,
+        englishWord: "education",
+        nepaliWord: "शिक्षा"
+      },
+      {
+        id: 2,
+        englishWord: "assessment",
+        nepaliWord: "मूल्यांकन"
+      },
+      {
+        id: 3,
+        englishWord: "adult migrant education program",
+        nepaliWord: "बयस्क प्रबासी शिक्षा कार्यक्रम"
+      }],
+    legal: [
+      {
+        id: 1,
+        englishWord: "legal",
+        nepaliWord: "कानूनी"
+      },
+      {
+        id: 2,
+        englishWord: "accused",
+        nepaliWord: "आरोपी"
+      },
+      {
+        id: 3,
+        englishWord: "access",
+        nepaliWord: "पौंच"
+      }],
+    health: [
+      {
+        id: 1,
+        englishWord: "health",
+        nepaliWord: "स्वास्थ्य"
+      },
+      {
+        id: 2,
+        englishWord: "agents",
+        nepaliWord: "तत्व"
+      },
+      {
+        id: 3,
+        englishWord: "adult",
+        nepaliWord: "बयस्क"
+      }],
+    finance: [
+      {
+        id: 1,
+        englishWord: "finance",
+        nepaliWord: "वित्त"
+      },
+      {
+        id: 2,
+        englishWord: "bank statement",
+        nepaliWord: "बैंक विवरण"
+      },
+      {
+        id: 3,
+        englishWord: "average",
+        nepaliWord: "औसत"
+      }],
+    immigration: [
+      {
+        id: 1,
+        englishWord: "immigration",
+        nepaliWord: "अध्यागमन"
+      },
+      {
+        id: 2,
+        englishWord: "business migration",
+        nepaliWord: "व्यापारिक आप्रवासन"
+      },
+      {
+        id: 3,
+        englishWord: "accomodation",
+        nepaliWord: "बस्ने ठाउँ"
+      }],
+    socialServices: [
+      {
+        id: 1,
+        englishWord: "social services",
+        nepaliWord: "सामाजिक सेवाहरु"
+      },
+      {
+        id: 2,
+        englishWord: "attached",
+        nepaliWord: "संलग्न"
+      },
+      {
+        id: 3,
+        englishWord: "appointment",
+        nepaliWord: "भेट्ने समय"
+      }],
+    consumerAffairs: [
+      {
+        id: 1,
+        englishWord: "consumer affairs",
+        nepaliWord: "उपभोक्ता मामिला"
+      },
+      {
+        id: 2,
+        englishWord: "consumer",
+        nepaliWord: "उपभोक्ता"
+      },
+      {
+        id: 3,
+        englishWord: "affairs",
+        nepaliWord: "मामिलाहरू"
+      }],
+    housing: [
+      {
+        id: 1,
+        englishWord: "housing",
+        nepaliWord: "आवास"
+      },
+      {
+        id: 2,
+        englishWord: "bedroom",
+        nepaliWord: "सुत्नेकोठा"
+      },
+      {
+        id: 3,
+        englishWord: "bathroom",
+        nepaliWord: "स्नान्कक्ष"
+      }],
+    employment: [
+      {
+        id: 1,
+        englishWord: "employment",
+        nepaliWord: "रोजगार"
+      },
+      {
+        id: 2,
+        englishWord: "amount",
+        nepaliWord: "रकम"
+      },
+      {
+        id: 3,
+        englishWord: "administrator",
+        nepaliWord: "प्रशासक"
+      }],
+    community: [
+      {
+        id: 1,
+        englishWord: "community",
+        nepaliWord: "समुदाय"
+      },
+      {
+        id: 2,
+        englishWord: "chores",
+        nepaliWord: "घराएसी कामहरु"
+      },
+      {
+        id: 3,
+        englishWord: "alcohol",
+        nepaliWord: "मादक पदार्थ"
+      }],
+    other: [
+      {
+        id: 1,
+        englishWord: "other",
+        nepaliWord: "अन्य"
+      },
+      {
+        id: 2,
+        englishWord: "actually",
+        nepaliWord: "बास्तवमा"
+      },
+      {
+        id: 3,
+        englishWord: "accordingly",
+        nepaliWord: "सोहि अनुसार"
+      }]
+  };
     const [englishWord, setEnglishWord] = useState("English Word");
     const [nepaliWord, setNepaliWord] = useState("नेपाली शब्द");
     const [counter, setCounter] = useState(0);
